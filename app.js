20c0a5b5967663f816567efbca9114e259d02027
Stephanie Jones <stephanie@Stephanies-MacBook-Pro-2.local>
2020-02-21 13:12:42 -0700

    some code

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const BASE_URL = 'https://dogs-backend.herokuapp.com/dogs'
+const dogsContainer = document.querySelector('.dogs-container')
+
+fetch(BASE_URL)
+    .then(response => response.json())
+    .then(dogs => {
+        dogs.map(dog => {
+            
+
+        })
+    })
