15c80e790d14a01e5537894849e52e711758aafd
Stephanie Jones <stephanie@Stephanies-MacBook-Pro-2.local>
2020-02-25 11:04:55 -0700

    Do it

--- app.js
+++ app.js
@@ -54,11 +54,12 @@ function editDog(id) {
     fetch(`${BASE_URL}/${id}`, {
         method: 'PATCH',
         headers: {
-            'Accept': 'application/json',
+            // 'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({ age })
-    })
+    }).then(parseJSON)
+        .then(console.log)
 }
 
 dogForm.addEventListener('submit', () => {
